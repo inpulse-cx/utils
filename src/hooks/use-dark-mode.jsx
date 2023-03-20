fddbf9a76e8756085417d0638ab785cc8e43dff1
Jo Santana <johnny@inpulse.digital>
2023-03-20 18:29:41 -0300

    fix: local-storage usage inside dark-mode hook

--- src/hooks/use-dark-mode.jsx
+++ src/hooks/use-dark-mode.jsx
@@ -1,14 +1,14 @@
-import { useEffect } from 'react';
-import useMediaQuery from './use-media-query';
-import { useLocalStorage } from './use-local-storage';
+import { useEffect } from "react";
+import useMediaQuery from "./use-media-query";
+import { useLocalStorage } from "./use-storage";
 
 function useDarkMode() {
-  const [darkMode, setDarkMode] = useLocalStorage('useDarkMode');
-  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const [darkMode, setDarkMode] = useLocalStorage("useDarkMode");
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
   const enabled = darkMode ?? prefersDarkMode;
 
   useEffect(() => {
-    document.body.classList.toggle('dark-mode', enabled);
+    document.body.classList.toggle("dark-mode", enabled);
   }, [enabled]);
 
   return [enabled, setDarkMode];
