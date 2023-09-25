8ff699dd28e2b05627970591d8108be7e2edd48d
Jo Santana <johnny.santana@polishop.com.br>
2023-09-25 18:09:10 -0300

    fix: update exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import * as hooks from "./hooks";
-import * as objects from "./objects";
-import * as sorting from "./sorting";
-import * as strings from "./strings";
-
-export type * as types from "./types";
-
-export default {
-  hooks,
-  objects,
-  sorting,
-  strings,
-};
+export * from "./hooks";
+export * from "./objects";
+export * from "./sorting";
+export * from "./strings";
+export * from "./types";
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,1 @@
-export type * from "./IndexedKey";
+export * from "./IndexedKey";
