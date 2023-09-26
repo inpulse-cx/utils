6f3bb167505cc03686385cc7ea289f523ed14300
Jo Santana <johnny@inpulse.digital>
2023-09-26 01:03:53 -0300

    fix: update  exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import { IndexedKey } from "./types";
-export * from "./hooks";
-export * from "./objects";
-export * from "./sorting";
-import * as strings from "./strings";
-
-export type { IndexedKey };
-
-export { strings };
-
-export default {
-  ...strings,
-};
+// export * from "./hooks";
+// export * from "./objects";
+// export * from "./sorting";
+export * from "./strings";
